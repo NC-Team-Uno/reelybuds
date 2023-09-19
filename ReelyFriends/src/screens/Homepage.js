1ad085bd990f2b7ea43de4d7bb952aa2579c74a8
haydenexley <haydenexley@outlook.com>
2023-09-19 16:28:50 +0100

    code review complete

--- ReelyFriends/src/screens/Homepage.js
+++ ReelyFriends/src/screens/Homepage.js
@@ -1,23 +1,25 @@
 import React, { useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, ScrollView, FlatList, Dimensions } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  FlatList,
+  Dimensions,
+} from "react-native";
 import { getNewMovies } from "../api/Apicall";
-import MovieCard from "../components/MovieCard"
-
-
+import MovieCard from "../components/MovieCard";
 
 const Homepage = () => {
   const [newMovies, setNewMovies] = useState({});
   useEffect(() => {
-    getNewMovies().then((movieResponse) =>
-    setNewMovies(movieResponse.data)
-    );
+    getNewMovies().then((movieResponse) => setNewMovies(movieResponse.data));
   }, []);
 
   return (
-
     <ScrollView style={styles.container}>
-      <StatusBar style="auto" translucent={false} />
+      <StatusBar style="dark" translucent={false} />
       <View style={styles.headerContainer}>
         <Text style={styles.headerSubtitle}>New Movies</Text>
       </View>
@@ -32,7 +34,7 @@ const Homepage = () => {
       />
     </ScrollView>
   );
-}
+};
 
 const deviceWidth = Dimensions.get("window").width;
 
@@ -54,7 +56,7 @@ const styles = StyleSheet.create({
   headerSubtitle: {
     fontSize: 13,
     color: "#FFF",
-    fontWeight: 'bold',
+    fontWeight: "bold",
     textTransform: "uppercase",
   },
   mainContainer: {
@@ -65,6 +67,5 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
   },
 });
-
 
 export default Homepage;
