75867336211dd10a81f406ca6dfad88d299ab1d4
Sula Hancock <sulahancock@sulas-mbp.home>
2023-09-20 17:02:15 +0100

    adds box to search friends

--- ReelyFriends/src/screens/FriendsScreen.js
+++ ReelyFriends/src/screens/FriendsScreen.js
@@ -1,10 +1,25 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
+import TextInputExample from "../components/FriendsSearchBar";
 
 export default function FriendsScreen(){
     return (
-        <View>
-            <Text> Friends Screen </Text>
+        <View style={styles.container}>
+            <Text style={styles.friends}> Friends Screen </Text>
+            <TextInputExample />
+            
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#1E2030",
+    },
+    friends: {
+        color: "#fff",
+        textAlign: "center",
+        fontSize: 25,
+    }
+})
