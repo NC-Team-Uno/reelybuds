715f4e9347cf6e0f4a7057b4bcc6161b316fa20d
Andra Moraru <andraimoraru>
2023-09-20 15:42:56 +0100

    adds some functionality to CreateProfile

--- ReelyFriends/App.js
+++ ReelyFriends/App.js
@@ -2,13 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Homepage from './src/screens/Homepage'
 import Header from './src/components/Header';
+import CreateProfile from './src/screens/CreateProfile';
 
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Header />
-      <Homepage />
+      <CreateProfile />
       <StatusBar style="auto" />
     </View>
   );
