d55cbb7aab8a2c53f904071714636f730ef111dc
haydenexley <haydenexley@outlook.com>
2023-09-21 14:14:01 +0100

    fixes app freeze on friends page

--- ReelyFriends/src/screens/FriendsScreen.js
+++ ReelyFriends/src/screens/FriendsScreen.js
@@ -2,24 +2,22 @@ import React from "react";
 import { View, Text, StyleSheet, Image } from "react-native";
 import TextInputExample from "../components/FriendsSearchBar";
 
-export default function FriendsScreen(){
-    return (
-        <View style={styles.container}>
-            <Text style={styles.friends}> Friends Screen </Text>
-            <FriendsScreen />
-            
-        </View>
-    )
+export default function FriendsScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.friends}> Friends Screen </Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#1E2030",
-    },
-    friends: {
-        color: "#fff",
-        textAlign: "center",
-        fontSize: 25,
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: "#1E2030",
+  },
+  friends: {
+    color: "#fff",
+    textAlign: "center",
+    fontSize: 25,
+  },
+});
