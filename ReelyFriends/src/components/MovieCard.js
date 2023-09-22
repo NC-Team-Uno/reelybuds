ed7276845f4b88a22861add55027a6e146695aee
haydenexley <haydenexley@outlook.com>
2023-09-22 13:40:00 +0100

    adds infinite scroll list for hardcoded provider data

--- ReelyFriends/src/components/MovieCard.js
+++ ReelyFriends/src/components/MovieCard.js
@@ -49,7 +49,7 @@ const styles = StyleSheet.create({
     elevation: 5,
     marginVertical: 20,
     marginBottom: 40,
-    marginHorizontal: 20,
+    marginHorizontal: 10,
     shadowColor: "#08080C",
     shadowOffset: {
       width: 10,
--- ReelyFriends/src/screens/MyList.js
+++ ReelyFriends/src/screens/MyList.js
@@ -1,27 +1,43 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import Icon from 'react-native-ionicons';
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import Icon from "react-native-ionicons";
+import { getAllMoviesForUser } from "../api/Apicall";
+import MovieCard from "../components/MovieCard";
 
 export default function MyList() {
-return (
-    <View style={styles.container}>
-        <Text style={styles.title}>My List</Text>
-  
+  const [movieList, setMovieList] = useState([]);
+  const [pageNo, setPageNo] = useState(1);
+  const endReached = () => {
+    setPageNo(pageNo + 1);
+  };
 
+  useEffect(() => {
+    getAllMoviesForUser(pageNo).then((movies) =>
+      setMovieList(movieList.concat(movies))
+    );
+  }, [pageNo]);
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={movieList}
+        onEndReached={endReached}
+        keyExtractor={(item) => item.id.toString()}
+        numColumns={3}
+        renderItem={({ item }) => <MovieCard item={item} />}
+      />
     </View>
-)
+  );
 }
 
-const styles = StyleSheet.create ({
-    container: {
-        flex: 1,
-        backgroundColor: "#1E2030"
-    },
-    title: {
-        color: "#fff",
-        textAlign: 'center',
-        fontSize: 25,
-        margin: 10,
-        
-    }
-})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#1E2030",
+  },
+  title: {
+    color: "#fff",
+    textAlign: "center",
+    fontSize: 25,
+    margin: 10,
+  },
+});
